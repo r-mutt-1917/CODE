e6477e4c4b89950da746ef7b1d62be46e4e5e105
Fran Simó <fsimo@hotelbeds.com>
2019-05-21 12:22:57 +0200

    CODE version 1.0 - docs

--- CODE.sh
+++ CODE.sh
@@ -3,3 +3,9 @@ sudo apt-get -y install enscript
 wget https://www.cybercom.net/~dcoffin/dcraw/dcraw.c
 
 enscript --pretty-print --color --language=html --line-numbers -p dcraw.c.html dcraw.c
+
+# install inconsolata fonts
+# from 1107.zip use A5_Template.docx
+# copy .html content into the document
+# activate numbering
+
