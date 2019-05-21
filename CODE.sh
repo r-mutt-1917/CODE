a61034d08dc0431b7c82c91200311f03838e644c
Fran Simó <fsimo@hotelbeds.com>
2019-05-21 12:12:13 +0200

    second colouring approach

--- CODE.sh
+++ CODE.sh
@@ -2,4 +2,4 @@ sudo apt-get -y install enscript
 
 wget https://www.cybercom.net/~dcoffin/dcraw/dcraw.c
 
-enscript  --line-numbers -p out-emacs.rtf  --language=rtf  --highlight=c --color=1 --style=emacs -c dcraw.c
+enscript --pretty-print --color --language=html --line-numbers -p dcraw.c.html dcraw.c
